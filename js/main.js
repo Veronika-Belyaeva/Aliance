47450feef8547d724b09d73921fad98a8597b9b4
Veronika-Belyaeva <b.v.2011@mail.ru>
2023-09-05 21:44:14 +0700

    fix: logo swither

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const navbar = document.querySelector('.navbar');
-const logo = document.querySelector('.logo-svg use');
+const logoLight = document.querySelector('.logo-light');
+const logo = document.querySelector('.logo');
 const mMenuToggle = document.querySelector('.mobile-menu-toggle');
 const menu = document.querySelector('.mobile-menu');
 
 const lightModeOn = (event) => {
   navbar.classList.add('navbar-light');
-  logo.href.baseVal = "img/sprite.svg#logo";
+  logo.style.display = 'block';
+  logoLight.style.display = 'none';
 };
 const lightModeOff = (event) => {
   navbar.classList.remove('navbar-light');
-  logo.href.baseVal = "img/sprite.svg#logo-light";
+  logo.style.display = 'none';
+  logoLight.style.display = 'block';
 };
 
 const openMenu = (event) => { // функция открывания меню
@@ -44,7 +47,7 @@ const swiper = new Swiper('.swiper', {
   },
   breakpoints: {
     // when window width is >= 320px
-    576: {
+    420: {
       slidesPerView: 2,
     },
     // when window width is >= 480px
