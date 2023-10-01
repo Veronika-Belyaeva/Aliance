b4d558e05c44b07df469a8c27a8d4d738387e55a
Veronika-Belyaeva <b.v.2011@mail.ru>
2023-10-01 14:42:52 +0700

    add: php include_once

---
+++ mobile-menu.php
@@ -0,0 +1,73 @@
+<div class="mobile-menu">
+      <ul class="mobile-menu-nav">
+        <li class="mobile-menu-nav-item">
+          <a href="/about.php" class="mobile-menu-link">О компании</a>
+        </li>
+        <li class="mobile-menu-nav-item">
+          <a href="/contract.php" class="mobile-menu-link">Контрактное производство</a>
+          <ul class="mobile-submenu">
+            <li class="mobile-submenu-item">
+              <a class="mobile-submenu-link" href="/product_avtohim.php">Автомобильная химия</a>
+            </li>
+            <li class="mobile-submenu-item">
+              <a class="mobile-submenu-link" href="#">Бытовая химия</a>
+            </li>
+            <li class="mobile-submenu-item">
+              <a class="mobile-submenu-link" href="#">Дезинфицирующие средства</a>
+            </li>
+            <li class="mobile-submenu-item">
+              <a class="mobile-submenu-link" href="#">Пищевые аэрозоли</a>
+            </li>
+            <li class="mobile-submenu-item">
+              <a class="mobile-submenu-link" href="#">Косметическая продукция</a>
+            </li>
+            <li class="mobile-submenu-item">
+              <a class="mobile-submenu-link" href="#">Краски аэрозольные</a>
+            </li>
+          </ul>
+        </li>
+        <li class="mobile-menu-nav-item">
+          <a href="#" class="mobile-menu-link">Собственные торговые марки</a>
+          <ul class="mobile-submenu">
+            <li class="mobile-submenu-item">
+              <a href="" class="mobile-submenu-link">Автохимия AG-Tech</a>
+            </li>
+            <li class="mobile-submenu-item">
+              <a href="" class="mobile-submenu-link">Автохимия AP</a>
+            </li>
+          </ul>
+        </li>
+        <li class="mobile-menu-nav-item">
+          <a href="#" class="mobile-menu-link">Новости</a>
+        </li>
+        <li class="mobile-menu-nav-item">
+          <a href="#" class="mobile-menu-link">Контакты</a>
+        </li>
+      </ul>
+      <a href="tel:+74996861014" class="mobile-phone">+7 (499) 686-10-14</a>
+      <div class="mobile-info">
+        <svg class="address-icon" height="24" width="24">
+          <use href="img/sprite.svg#mark"></use>
+        </svg> 
+        <address class="mobile-info-address">г. Мосвка, Холодильный пер. 4к1с8</address>
+      </div>
+      <div class="mobile-info">
+        <svg class="address-icon" height="24" width="24">
+          <use href="img/sprite.svg#mail"></use>
+        </svg> 
+        <a href="mailto:a.dragunov@tdaliance.ru" class="mobile-info-email">a.dragunov@tdaliance.ru</a>
+      </div>
+      <!-- /.mobile-info -->
+      <div class="mobile-info">
+        <a href="#" class="mobile-info-icon">
+          <svg class="address-icon" height="24" width="24">
+            <use href="img/sprite.svg#vk"></use>
+          </svg> 
+        </a>
+        <a href="#" class="mobile-info-icon">
+          <svg class="address-icon" height="24" width="24">
+            <use href="img/sprite.svg#inst"></use>
+          </svg> 
+        </a>
+      </div>
+    </div>
---
+++ modal.php
@@ -0,0 +1,52 @@
+<div class="modal">
+      <div class="modal-dialog">
+        <h2 class="modal-title">Есть вопросы?</h2>
+        <a href="#" class="modal-close" data-toggle="modal">
+          <svg class="close-icon" height="20" width="20">
+            <use href="img/sprite.svg#close"></use>
+          </svg> 
+        </a>
+        <p class="modal-text">
+          Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время ответит на все интересующие вопросы и поможем даже в самых сложных случаях! 
+        </p>
+        <form action="#" class="modal-form">
+          <div class="input-group-wrapper input-group-vertical">
+            <div class="input-group modal-input-group">
+              <input 
+              id="modal-user-name" 
+              type="text" 
+              class="input modal-input"
+              name="username"
+              placeholder=" "
+              >
+              <label class="input-group-label modal-input-label" for="modal-user-name">Имя</label>
+            </div>
+            <!-- /.input-group -->
+            <div class="input-group modal-input-group">
+              <input 
+              id="modal-user-phone" 
+              type="text" 
+              class="input modal-input"
+              name="user-phone"
+              placeholder=" "
+              >
+              <label class="input-group-label modal-input-label" for="modal-user-phone">Номер телефона</label>
+            </div>
+            <!-- /.input-group -->
+          </div>
+          <!-- /.input-group-wrapper -->
+          <div class="modal-form-footer">
+            <button type="submit" class="button modal-form-button">Отправить заявку</button>
+            <div class="notify">
+              <svg class="notify-icon" height="14" width="14">
+                <use href="img/sprite.svg#shield"></use>
+              </svg> 
+              <p class="notify-text">
+                Обращаясь к нам вы получаете не только профессиональную работу, но и абсолютную конфиденциальность информации!
+              </p>
+            </div>
+          </div>
+          <!-- /.modal-form-footer -->
+        </form>
+      </div>
+    </div>
